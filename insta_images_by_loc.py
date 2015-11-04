f595a5f002ff6dd391c1091883c8210fa533c5d2
Dhruv Thakur <dhruvt93@gmail.com>
2015-11-04 17:36:39 +0530

    Update insta_images_by_loc.py

--- insta_images_by_loc.py
+++ insta_images_by_loc.py
@@ -7,17 +7,16 @@ client_secret="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"                          #clien
 redirect_uri="http://localhost"                                           #redirect uri. I've set mine to localhost. This can be changed
 grant_type="authorization_code"                                           #default option provided by Instagram
 
-req_params={"client_id":client_id, "client_secret":client_secret, \
-"redirect_uri":redirect_uri, "grant_type":grant_type, "code":code}
-
-def get_access_token(req_params):
+def get_access_token():
     auth_url="https://api.instagram.com/oauth/authorize/?client_id=" + client_id + "&redirect_uri=" + redirect_uri + "&response_type=code"
     print auth_url
     print "Take this url and paste in your browser. Authorize the app: "+auth_url    #authorization step. need a better way to do this
 
     redirected_url=raw_input("Now enter the url returned after authorization.\n")    #browser will redirect to a new page whose url will have the code as a response
     code=redirected_url.split('code=')[1]
-
+    
+    req_params={"client_id":client_id, "client_secret":client_secret, "redirect_uri":redirect_uri, "grant_type":grant_type, "code":code}
+    
     request_url="https://api.instagram.com/oauth/access_token"
     r=requests.post(request_url, data=req_params)                      #making a POST request with the client details as parameters
 
@@ -44,4 +43,5 @@ c=coordinates(location)
 lat=str(c[0])
 lng=str(c[1])
 
-media_search(lat,lng, get_access_token(req_params))
+access_token=get_access_token()
+media_search(lat, lng, access_token)
